46d700c49542de8ca38324dc258dcbf07b101b50
abhishekg-21 <asgdon1@example.com>
2025-07-12 01:41:10 +0530

    Fix: Added missing dependency to useEffect in ScrollToTop

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,7 +18,8 @@ const projects: IProject[] = [
     ],
     githubLink: "https://github.com/abhishekg-21", // Replace with actual GitHub link
     liveDemoLink: "https://e-shop-frontend-eta.vercel.app/", // Replace with actual live demo link (if exists)
-    image: "srcdataE-Shop.png", // Path to project screenshot
+    image:
+      "https://github.com/abhishekg-21/MYPORTFOLIO/blob/main/src/data/LMS.png", // Path to project screenshot
   },
   {
     id: "qr-code-lms-module",
@@ -36,7 +37,8 @@ const projects: IProject[] = [
     ],
     githubLink: "https://github.com/abhishekg-21", // Replace with actual GitHub link
     liveDemoLink: "https://scans-gold.vercel.app/", // Replace with actual live demo link (if exists)
-    image: "srcdataLMS.png", // Path to project screenshot
+    image:
+      "https://github.com/abhishekg-21/MYPORTFOLIO/blob/main/src/data/LMS.png", // Path to project screenshot
   },
   // Add more projects as Abhishek develops them
   // {
